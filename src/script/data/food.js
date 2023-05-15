0a5ba999c91bd90a7e49008de2a29afe658c456d
Andito Oktano Putra <84326871+OktanoPutra@users.noreply.github.com>
2023-05-15 13:35:00 +0700

    Add files via upload

---
+++ src/script/data/food.js
@@ -0,0 +1,39 @@
+const foods = [
+  {
+    name: 'Beef Wellington',
+    foto: 'https://www.themealdb.com/images/media/meals/vvpprx1487325699.jpg',
+    description: 'Beef fillet yang digulung ham dan pastry',
+  },
+  {
+    name: 'Strawberry Rhubarb Pie',
+    foto: 'https://www.themealdb.com/images/media/meals/178z5o1585514569.jpg',
+    description: 'Pie dengan isi paduan dari strawberry dan rhubarb',
+  },
+  {
+    name: 'Sushi',
+    foto: 'https://www.themealdb.com/images/media/meals/g046bb1663960946.jpg',
+    description: 'Nasi gulung dari jepang berisikan wortel timun dan ikan mentah segar',
+  },
+  {
+    name: 'Tonkatsu Pork',
+    foto: 'https://www.themealdb.com/images/media/meals/lwsnkl1604181187.jpg',
+    description: 'Pork yang dibaluri tepung roti dan digoreng kemudian disiram kuah soyu',
+  },
+  {
+    name: 'Yaki Udon',
+    foto: 'https://www.themealdb.com/images/media/meals/wrustq1511475474.jpg',
+    description: 'Mie goreng ala jepang',
+  },
+  {
+    name: 'Lasagne',
+    foto: 'https://www.themealdb.com/images/media/meals/wtsvxx1511296896.jpg',
+    description: 'Pasta berlapis dari Italia',
+  },
+  {
+    name: 'Pizza Express Margherita',
+    foto: 'https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg',
+    description: 'Pizza margherita bertopping mozzarela dan basil',
+  }
+];
+
+export default foods;
