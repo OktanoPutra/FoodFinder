18192dfa7fcf92ba25f02d74905ca14b42853efb
Andito Oktano Putra <84326871+OktanoPutra@users.noreply.github.com>
2023-05-15 14:19:00 +0700

    Update data-source.js
    update fetch JSON

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,6 +1,6 @@
 class DataSource {
   static searchFood(keyword) {
-    return fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`)
+    return fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`)
         .then(response => {
           return response.json();
         })
